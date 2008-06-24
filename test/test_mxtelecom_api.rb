574ebfab7c3bbbdb688dfbb7811dea08ff256c6d
Jim Garvin <jim@thegarvin.com>
2008-06-24 11:11:22 -0400

    Fixed warnings by adding parens around method args.

--- test/test_mxtelecom_api.rb
+++ test/test_mxtelecom_api.rb
@@ -10,16 +10,16 @@ class TestMxtelecomApi < Test::Unit::TestCase
 
   def test_url_construction
     url = TestMxtelecomApi.setup_sms_params('123-123-1234', 'Call me!')
-    assert_match /^\/SMSSend?/, url
-    assert_match /smsto=1231231234/, url
-    assert_match /pass=password1/, url
-    assert_match /user=bobbymcgee/, url
-    assert_match /smsfrom=12345/, url
-    assert_match /smsmsg=Call%20me!/, url
+    assert_match(/^\/SMSSend?/, url)
+    assert_match(/smsto=1231231234/, url)
+    assert_match(/pass=password1/, url)
+    assert_match(/user=bobbymcgee/, url)
+    assert_match(/smsfrom=12345/, url)
+    assert_match(/smsmsg=Call%20me!/, url)
     
     url = TestMxtelecomApi.setup_sms_params('123-123-1234', 'Call me!', :user => "foobar", :pass => "barbaz")
-    assert_match /pass=barbaz/, url
-    assert_match /user=foobar/, url
+    assert_match(/pass=barbaz/, url)
+    assert_match(/user=foobar/, url)
   end
   
   def test_delivery
